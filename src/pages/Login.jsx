a96c0a24325e8936a0c1bf8d9683626876499c27
Adebiyi Qudus <adebiyiqudus2018@gmail.com>
2026-08-04 02:36:40 +0100

    Dev Email

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,7 +4,7 @@ import styles from "./Login.module.css";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
-  const [email, setEmail] = useState("jack@example.com");
+  const [email, setEmail] = useState("qudus@gmail.com");
   const [password, setPassword] = useState("qwerty");
 
   return (
