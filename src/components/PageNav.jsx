d6747221613500df750faca78bc504098c721a2e
Adebiyi Qudus <adebiyiqudus2018@gmail.com>
2026-08-04 02:34:51 +0100

    Components Updates

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,18 +1,23 @@
 import { NavLink } from "react-router-dom"
+import Logo from "./Logo"
 import styles from "./pageNav.module.css"
 
 function PageNav() {
   return (
     <nav className={styles.nav}>
-      <ul>
-        <li>
-      <NavLink to="/">Home</NavLink>
-      </li>
+      <Logo />
+
+      <ul> 
       <li>
       <NavLink to="/pricing">Pricing</NavLink>
       </li>
       <li>  
       <NavLink to="/product">Product</NavLink>
+    </li>
+      <li>  
+      <NavLink to="/login" className={styles.ctaLink}>
+        Login
+      </NavLink>
     </li>
       </ul>
     </nav>
