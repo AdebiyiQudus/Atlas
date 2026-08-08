c7cc2fb0e4c98709e1ad9cebe72df532de04997f
Adebiyi Qudus <adebiyiqudus2018@gmail.com>
2026-08-08 23:27:21 +0100

    Nested Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // Altanative to styling with CSS: UI libraries like Tailwind CSS, Material UI, Mantine, Chakra UI, Bootstrap
-
+// <Outlet/> component acts as a placeholder or slot within a parent route's layout. It dictates where nested child route components should be rendered based on the current URL path.
+// <Route index /> is for defining the default child route of a parent route. It specifies which component should be rendered when the parent route is accessed without any additional path segments.
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
@@ -13,11 +14,21 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/product" element={<Product />} />
         <Route path="/login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+      // Default route for the "app" whenever the app page is rendered or when no specific child route is provided
+          <Route index element={<p>Welcome to the app</p>} />
+
+      // Nested routes for the "app" path
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+
+        </Route>
+
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,11 +1,19 @@
+import { NavLink } from "react-router-dom"
 import styles from "./AppNav.module.css"
 // import {nav} from "./AppNav.module.css"
 
 function AppNav() {
   return (
-    <div>
-      App Navigation
-    </div>
+    <nav className={styles.nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+      </nav>
 
     // <nav className={nav}>App Navigation</nav>
   )
