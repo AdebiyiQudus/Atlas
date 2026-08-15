0090799bc9f868e4793bb3c11284bb27cbe29544
Adebiyi Qudus <adebiyiqudus2018@gmail.com>
2026-08-15 23:04:46 +0100

    City Message

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,10 +1,17 @@
 import Spinner from './Spinner'
 import styles from './CityList.module.css'
 import CityItem from './CityItem'
+import Message from './Message'
 
 function CityList({ citiesProp, isLoadingProp }) {
 
   if (isLoadingProp) return <Spinner />
+
+  if (!citiesProp.length)
+    return (
+     <Message message="Add your first city by clicking a
+      city on the map" />
+    )
 
   return (
     <ul className={styles.cityList}>
