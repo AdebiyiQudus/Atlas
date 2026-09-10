404b0d156d51097b501bf4b644fc8960fc135338
Adebiyi Qudus <adebiyiqudus2018@gmail.com>
2026-09-10 02:07:49 +0100

    Lat anf Lng

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -8,6 +8,7 @@ import styles from './Map.module.css'
 function Map() {
   const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
+  
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
 
