8ec2500c8cb534c82b2fa109b1827665ababd727
Adebiyi Qudus <adebiyiqudus2018@gmail.com>
2026-09-24 02:21:28 +0100

    Children component

--- src/components/CitiesContext.jsx
+++ src/components/CitiesContext.jsx
@@ -4,25 +4,26 @@ const BASE_URL = "http://localhost:9000";
 
 const CitiesContext = createContext();
 
-const CitiesContextProvider = ({ children }) => { 
- const [cities, setCities] = useState([]);
-  const  [isLoading, setIsLoading] = useState(false);
+const CitiesProvider = ({ children }) => {
 
-  useEffect (function() {
-    async function fetchCities() {
-      try {
-      setIsLoading(true);
-
-      const res = await fetch(`${BASE_URL}/cities`)
-      const data = await res.json();
-
-      setCities(data);
-      } catch {
-        alert("There was an error loading data...");
-      } finally {
-        setIsLoading(false);
-      }
-    }
-    fetchCities();
-  }, [])
+  const [cities, setCities] = useState([]);
+   const  [isLoading, setIsLoading] = useState(false);
+ 
+   useEffect (function() {
+     async function fetchCities() {
+       try {
+       setIsLoading(true);
+ 
+       const res = await fetch(`${BASE_URL}/cities`)
+       const data = await res.json();
+ 
+       setCities(data);
+       } catch {
+         alert("There was an error loading data...");
+       } finally {
+         setIsLoading(false);
+       }
+     }
+     fetchCities();
+   }, [])
 }
